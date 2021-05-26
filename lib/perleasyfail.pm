895c941abf9f2b9e1ba52ddef444437f5757daa9
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-05-26 09:09:13 +0700

    Initial commit

---
+++ lib/perleasyfail.pm
@@ -0,0 +1,9 @@
+package perleasyfail;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: A collection of cases where core Perl fails its "easy things should be easy" mantra (plus their remedies)
